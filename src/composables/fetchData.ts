d652200ea0b2359b877a7ef2bb8addada3a9f90f
Yuma Morimoto <kanten520212@gmail.com>
2024-03-29 22:38:07 +0900

    Feat: add microcms api

---
+++ src/composables/fetchData.ts
@@ -0,0 +1,87 @@
+import type {
+  MicroCMSImage,
+  MicroCMSDate,
+  MicroCMSListResponse,
+} from 'microcms-js-sdk';
+
+interface Concept {
+  fieldId: string;
+  concept1Text: string;
+  concept1Image: MicroCMSImage;
+  concept2Text: string;
+  concept2Image: MicroCMSImage;
+}
+
+interface Stylist {
+  fieldId: string;
+  nameJa: string;
+  nameEn: string;
+  headerText: string;
+  bodyText: string;
+  thumbnail: MicroCMSImage;
+}
+
+interface Access {
+  fieldId: string;
+  address: string;
+  tel: string;
+  email: string;
+  open: string;
+  close: string;
+  other: string;
+}
+
+type Settings = {
+  catchcopy: string;
+  concept: Concept;
+  gallery: MicroCMSImage[];
+  stylist: Stylist;
+  access: Access;
+  hotpepperUrl: string;
+  lineUrl: string;
+} & MicroCMSDate;
+
+interface MenuItem {
+  attr: string[];
+  name: string;
+  price: string;
+}
+
+interface BlogListItem {
+  title: string;
+  postAt: string;
+  contents: string;
+  thumbnail: MicroCMSImage;
+}
+
+type Menu = MicroCMSListResponse<MenuItem>;
+type BlogList = MicroCMSListResponse<BlogListItem>;
+
+export const fetchHome = async (): Promise<Settings> => {
+  const res = await useMicroCMSGetObject({ endpoint: 'settings' });
+  const data: Settings = res.data.value;
+  return data;
+};
+
+export const fetchMenu = async (): Promise<Menu | null> => {
+  const res = await useMicroCMSGetList<MenuItem>({
+    endpoint: 'menu',
+    queries: { limit: 100 },
+  });
+  const data: Menu | null = res.data.value;
+  return data;
+};
+
+export const fetchBlogList = async (): Promise<BlogList | null> => {
+  const res = await useMicroCMSGetList<BlogListItem>({
+    endpoint: 'blog',
+    queries: {
+      limit: 12,
+      orders: '-createdAt',
+    },
+  });
+  const data: BlogList | null = res.data.value;
+  return data;
+};
+
+export const fetchBlog = () => {};
